08c667d830630bc8a75e34aee0b55883f6c98d26
HarshitaSindhu <harshitasindhu5120@gmail.com>
2025-07-20 16:06:41 +0530

    Time: 3 ms (38.65%), Space: 42.3 MB (8.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,27 @@
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+    public boolean isValid(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            
             if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
+                st.push(c);
             } else {
-                if (stack.isEmpty()) {
+                if (st.isEmpty()) {
                     return false;
+                }
+
+                if ((st.peek() == '(' && c == ')') ||
+                    (st.peek() == '{' && c == '}') ||
+                    (st.peek() == '[' && c == ']')) {
+                    st.pop();  
                 } else {
-                    char top = stack.pop();
-                    if ((c == ')' && top != '(') ||
-                        (c == '}' && top != '{') ||
-                        (c == ']' && top != '[')) {
-                        return false;
-                    }
+                    return false;  
                 }
             }
         }
-        return stack.isEmpty(); 
+        return st.isEmpty();
     }
 }
+
