e53888da28cd5508766cc0ef88393425661020cb
HarshitaSindhu <harshitasindhu5120@gmail.com>
2025-07-23 13:03:38 +0530

    Time: 2 ms (99.2%), Space: 43.1 MB (95.55%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int result[] = new int[nums1.length];
+        for(int i = 0;i<nums1.length;i++){
+          int num = nums1[i];
+          int indexInNums2= -1;
+
+          for(int j =0;j<nums2.length;j++){
+            if(nums2[j]== num){
+                indexInNums2 = j;
+                break;
+            }
+          }
+          int nextGreater = -1;
+          for(int j = indexInNums2+1;j<nums2.length;j++){
+            if(nums2[j]>num){
+                nextGreater = nums2[j];
+                break;
+            }
+          }
+          result[i]= nextGreater;
+        }
+        return result;
+    }
+}
