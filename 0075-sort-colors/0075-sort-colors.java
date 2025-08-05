1f582371d5e209ae27504ed57d27e611e57ee25f
HarshitaSindhu <harshitasindhu5120@gmail.com>
2025-08-05 11:39:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,35 +1,37 @@
 class Solution {
-    public void sortColors(int[] nums) 
-    {
-      int countZero = 0;
-      int countOne = 0;
-      int countTwo = 0;
-      int n = nums.length;
+    public void sortColors(int[] nums) {
+     int n = nums.length;
+     int countZero = 0;
+     int countOne = 0;
+     int countTwo = 0;
 
-      for(int i = 0;i<n;i++){
-        if(nums[i]==0){
+     for(int i= 0;i<n ;i++){
+        if(nums[i]== 0){
             countZero++;
         }
-        if(nums[i]==1){
+        else if(nums[i]==1){
             countOne++;
         }
-        if(nums[i]==2){
+        else if (nums[i] == 2){
             countTwo++;
         }
-      }
-
-      int j = 0;
-      for(int i = 0;i<countZero;i++){
-        nums[j]=0;
-        j++;
-      }
-      for(int i = 0;i<countOne;i++){
-        nums[j]=1;
-        j++;
-      }for(int i = 0;i<countTwo;i++){
-        nums[j]=2;
-        j++;
-      }
-     
      }
+        int j = 0;
+
+        for(int k = 0;k<countZero;k++){
+            nums[j] = 0;
+            j++;
+        }
+
+        
+        for(int k =0;k<countOne;k++){
+            nums[j] = 1;
+            j++;
+        }
+        for(int k =0;k<countTwo;k++){
+            nums[j] = 2;
+            j++;
+        }
+     
+    }
 }
