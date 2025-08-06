acce2cde96ec783335e41c549c12fe634e36bc3c
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-06 14:39:02 +0530

    Time: 0 ms (100%), Space: 45.2 MB (9.56%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -8,52 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- class Solution {
+class Solution {
     public ListNode oddEvenList(ListNode head) {
         ListNode oH = null;
-        ListNode oT = null;
-        
         ListNode eH = null;
+        ListNode oT = null;
         ListNode eT = null;
-        
         ListNode temp = head;
-        
+
         if(head == null){
-          return head;
+            return head;
         }
-        
+
         int index = 1;
         while(temp != null){
-          if(index %2 ==0){
-            if(eH == null && eT == null){
-              eH = temp;
-              eT = temp;
+            if(index %2 == 0){
+                if(eH == null && eT == null){
+                    eH = temp;
+                    eT = temp;
+                }
+                else{
+                    eT.next = temp ;
+                    eT = temp;
+                }
             }
             else{
-              eT.next =  temp;
-              eT= temp;
+                if(oH == null && oT == null){
+                    oH = temp;
+                    oT = temp;
+                }
+                else{
+                oT.next = temp;
+                oT = temp;
             }
-            
-          }
-          else{
-            if(oH == null && oT == null){
-              oH = temp;
-              oT = temp;
-              
             }
-            else{
-              oT.next =  temp;
-              oT = temp;
-            }
-          }
-          index++;
-          temp=temp.next;
-        }
-        oT.next = eH;
-        if(eT != null){
-          eT.next =null;
-        }
-        
-        return oH;
+        index++;
+        temp = temp.next;
+    }
+
+      oT.next = eH;
+      if(eT != null){
+        eT.next  = null;
+      }
+      return oH;
     }
 }
