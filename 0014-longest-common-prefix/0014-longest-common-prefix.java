244ac5de5f901e70657c28ad360e4be3d37488b9
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-08 12:49:47 +0530

    Time: 1 ms (62.74%), Space: 41.5 MB (62.36%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        Arrays.sort(strs);  
+        String str1 = strs[0];
+        String str2 = strs[strs.length - 1];
+        
+        int index = 0;
+        int minLength = Math.min(str1.length(), str2.length());
+        
+        while (index < minLength && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        
+        return str1.substring(0, index);
+    }
+}
