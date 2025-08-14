739eced68db29c0bc2bdf36b5ce8450ee7e7cfcb
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-14 22:16:39 +0530

    Time: 72 ms (14.43%), Space: 58.8 MB (47.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -16,3 +16,40 @@ class Solution {
         return q.peek();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// nums = [9, 5, 2, 7]
+// k = 3
+// Step 1 — Make max heap:
+// Heap looks like (largest first):
+
+
+// [9, 7, 5, 2]
+// Step 2 — First k--:
+
+// k = 3
+// k--  // now k = 2 → means "remove 2 numbers"
+// Step 3 — While loop:
+
+
+// while (k > 0) {     // k = 2 → yes
+//     q.remove();     // removes 9
+//     k--;            // now k = 1
+// }
+
+// while (k > 0) {     // k = 1 → yes
+//     q.remove();     // removes 7
+//     k--;            // now k = 0
+// }
+
+// while (k > 0) {     // k = 0 → no, stop
+// }
