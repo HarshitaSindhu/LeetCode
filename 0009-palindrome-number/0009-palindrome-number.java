2cf364fbdf33a15ff32da7892ff5607ed5c483c7
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-24 15:07:35 +0530

    Time: 4 ms (100%), Space: 43.8 MB (88.93%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x<0){
+            return false;
+        }
+        int original = x;
+        int rev = 0;
+        while(x!= 0){
+            int lastDigit = x%10;
+            rev = rev*10+lastDigit;
+            x = x/10;
+
+        }
+        if(rev == original){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
