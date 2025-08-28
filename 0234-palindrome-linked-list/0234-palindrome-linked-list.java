95094af040b2f5e73c3680fc523aedc0699f1704
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-28 18:42:29 +0530

    Time: 4 ms (83.87%), Space: 68.4 MB (52.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,45 +9,40 @@
  * }
  */
 class Solution {
-    public static ListNode reverse(ListNode head){
+     public static ListNode reverse(ListNode head){
+        ListNode prev =null;
+        ListNode temp =head;
+        while(temp!=null){
+            ListNode nxt = temp .next ;
+            temp.next=prev;
+            prev = temp;
+            temp =nxt;
 
-        ListNode temp = head;
-        ListNode prev = null;
-
-        while(temp!= null){
-            ListNode nxt = temp.next;
-            temp.next = prev;
-            prev = temp ;
-            temp = nxt;
         }
         return prev;
+
     }
     public boolean isPalindrome(ListNode head) {
-        ListNode temp = head;
-        ListNode slow = head;
+      
+        ListNode slow  = head;
         ListNode fast = head;
-           if (head == null || head.next == null) return true;
+        while (fast!=null && fast.next!=null){
+            slow =slow.next;
+            fast=fast.next.next
+            ;
 
-
-        while(fast!= null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
 
         }
-        ListNode mid = slow;
-        
-       
-
-        ListNode secondHalfHead = reverse(mid);
+        ListNode currentNode =reverse(slow);
         ListNode i = head;
-        ListNode j = secondHalfHead;
-        while(j != null ){
-            if(i.val != j.val){
-                return false; 
+        ListNode j=currentNode;
+        while(j!=null){
+            if(i.val!=j.val){
+                return false ;
             }
-            i = i.next;
-            j= j.next;
+            i=i.next;
+            j=j.next;
         }
-        return true;
+        return true ;
     }
 }
