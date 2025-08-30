6314b686204b68c26ec74d2a287fe2669674cfbb
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-30 18:34:52 +0530

    Time: 1 ms (100%), Space: 42.2 MB (91.06%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,39 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0;
-        int  n = s.length();
-        int sign = 1;
-        long result = 0;
-        long num = 0;
 
-        while(i<n && s.charAt(i)==' '){
-            i++;
+      int i = 0;
+      int sign = 1;
+      long num  = 0;
+      int n = s.length();
+      while(i<n && s.charAt(i)==' '){
+        i++;
+      }
+      if(i<n && (s.charAt(i)=='+'|| s.charAt(i) =='-')){
+        if(s.charAt(i)=='-'){
+            sign = -1;
         }
+        i++;
+      }
 
-        if(i<n &&(s.charAt(i) == '+' || s.charAt(i) == '-')){
-            if(s.charAt(i)=='-'){
-                sign = -1;
-            }
-            i++;
+       while(i<n){
+        char ch = s.charAt(i);
+        if(ch <'0'|| ch>'9'){
+            break;
         }
-        while(i<s.length()){
-            char ch = s.charAt(i);
-            if( ch <'0' || ch>'9'){
-                break;
-            }
-            num = num *10 +(ch-'0');
-            if (sign * num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign * num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-            i++;
+        num = num*10+ (ch-'0');
+        if((num*sign) >Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
         }
+        if((num*sign)<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+            
+        }
+        i++;
+       }
 
+     
         
         return (int)(sign * num);
+
     }
 }
