b88a2d16879ed0ed42dbb2f878975f3ed487b457
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-31 16:10:31 +0530

    Time: 268 ms (26.75%), Space: 45.4 MB (7.22%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        String repeated = a;
+
+        int count =1;
+
+        while(repeated.length()<b.length()){
+            repeated += a;
+            count++;
+        }
+
+        if(repeated.contains(b)){
+            return count;
+        }
+        repeated+=a;
+        count++;
+        if(repeated.contains(b)){
+            return count;
+        }
+        return -1;
+    }
+}
