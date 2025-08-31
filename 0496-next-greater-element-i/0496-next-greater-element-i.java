7fe3b315e5b9cc4de7360ff17234fe6586deff8a
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-08-31 23:08:57 +0530

    Time: 3 ms (91.23%), Space: 43.4 MB (76.83%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int result[]= new int [nums1.length];
-        for(int i =0;i<nums1.length;i++){
-            int num=nums1[i];
-            int indexnums2=-1;
-            for(int j=0;j<nums2.length;j++){
+        int result[] = new int[nums1.length];
+
+        for(int i = 0;i<nums1.length;i++){
+            int num  = nums1[i];
+            int indexinNums2 = -1;
+
+            for(int j = 0;j<nums2.length;j++){
                 if(nums2[j]==num){
-                    indexnums2=j;
+                    indexinNums2 = j;
                     break;
-
                 }
             }
-            int nextgreater =-1;
-            for(int k=indexnums2+1;k<nums2.length;k++){
+               int nextGreater  = -1;
+               for(int k = indexinNums2 +1 ; k<nums2.length;k++){
                 if(nums2[k]>num){
-                    nextgreater=nums2[k];
+                   nextGreater = nums2[k];
                     break;
                 }
-                
-
-            }
-            result[i]=nextgreater;
+               }
+            
+               result[i] = nextGreater;
+            
         }
         return result;
     }
-    }
-
+}
