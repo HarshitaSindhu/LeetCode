a03f144d6b9b2a4d993ad4e6d50730ed5c0325c8
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-09-10 13:20:32 +0530

    Time: 94 ms (39.27%), Space: 58.2 MB (16.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,56 +1,70 @@
 class Solution {
 
-public int[] nextSmallerLeft(int heights[]){
-    int N= heights.length;
-    int answer[] = new int[N];
-    Arrays.fill(answer ,-1);
-    Stack<Integer> st = new Stack<>();
-       for(int i = N-1;i>=0;i--){
-          while(!st.isEmpty()&& heights[st.peek()]>heights[i]){
-            answer[st.peek()] = i;
-            st.pop();
-          }
-          st.push(i);
-       }
-       return answer;
-
-}
-
-
-
-     public int [] nextSmallerRight(int []heights){
-        int answer[]= new int[heights.length];
-                int N= heights.length;
-
-        Arrays.fill(answer,N);
+    public int[] nextSmallerLeft(int heights[]){
+        int N = heights.length;
+        int answer[] = new int[N];
+        Arrays.fill(answer, -1);
         Stack<Integer> st = new Stack<>();
-
-        for(int i = 0;i<N;i++){
-
-            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
-                answer[st.peek()]  = i ;
+        for(int i = N - 1; i >= 0; i--){
+            while(!st.isEmpty() && heights[st.peek()] > heights[i]){
+                answer[st.peek()] = i;
                 st.pop();
             }
             st.push(i);
-
-
         }
         return answer;
-        
     }
 
-
-    public int largestRectangleArea(int[] heights) {
+    public int[] nextSmallerRight(int heights[]){
         int N = heights.length;
-        int left[]= nextSmallerLeft(heights);
+        int answer[] = new int[N];
+        Arrays.fill(answer, N);
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < N; i++){
+            while(!st.isEmpty() && heights[st.peek()] > heights[i]){
+                answer[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        return answer;
+    }
+
+    public int largestRectangleArea(int[] heights){
+        int N = heights.length;
+        int left[] = nextSmallerLeft(heights);
         int right[] = nextSmallerRight(heights);
 
         int answer = 0;
-        for(int i = 0;i<N;i++){
-            int width = right[i]-left[i]-1;
-            int area = width*heights[i];
-            answer = Math.max(answer , area);
+        for(int i = 0; i < N; i++){
+            int width = right[i] - left[i] - 1;
+            int area = width * heights[i];
+            answer = Math.max(answer, area);
         }
         return answer;
+    }
+
+    public int maximalRectangle(char[][] matrix){
+        int N = matrix.length;
+        if(N == 0) return 0;
+        int M = matrix[0].length;
+        int arr[][] = new int[N][M];
+        int maxArea = 0;
+
+        for(int i = 0; i < N; i++){
+            int sum = 0;
+            for(int j = 0; j < M; j++){  
+                if(matrix[i][j] == '1'){
+                    if(i == 0)
+                        arr[i][j] = 1;
+                    else
+                        arr[i][j] = arr[i-1][j] + 1;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(arr[i]));
+        }
+        return maxArea;  
     }
 }
