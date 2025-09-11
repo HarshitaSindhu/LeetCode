b71b0a2c8d6f216e22f8558fe47fe120d2d50058
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-09-11 18:54:41 +0530

    Time: 1 ms (56.59%), Space: 41.5 MB (83.33%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
+     Stack<Character>  st = new Stack<>();
+     for(int i = 0;i<s.length();i++){
+        char ch = s.charAt(i);
+        if(ch == '('){
+            st.push(ch);
 
-        for(int i = 0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c =='('){
-                st.push(c);
+        }
+        else{
+            if( !st.isEmpty()&&st.peek()=='('){
+                st.pop();
             }
             else{
-                if(!st.isEmpty()&& st.peek()=='('){
-                    st.pop();
-                }
-                else{
-                    st.push(c);
-                }
+                st.push(ch);
             }
         }
-        return st.size();
+     }
+     return st.size();
     }
 }
