61bfe3d007c820a6e8700646420b48abfb497d65
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-09-12 00:42:32 +0530

    Time: 1 ms (100%), Space: 46.3 MB (27.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -5,39 +5,33 @@ class Solution {
         int k = 0;
         int[] result = new int[nums1.length + nums2.length];
         
-        // Merge the two arrays
         while(i < nums1.length && j < nums2.length) {
             if(nums1[i] < nums2[j]) {
                 result[k] = nums1[i];
                 i++;
                 k++;
-            } else {  // This was wrong in your code (used if instead of else)
+            } else {  
                 result[k] = nums2[j];
                 j++;
                 k++;
             }
         }
 
-        // Copy remaining elements from nums1
         while(i < nums1.length) {
-            result[k] = nums1[i];  // Fixed: was 'nums[i]' in your code
+            result[k] = nums1[i]; 
             i++;
             k++;
         }
 
-        // Copy remaining elements from nums2
         while(j < nums2.length) {
-            result[k] = nums2[j];  // Fixed: was 'nums[j]' in your code
+            result[k] = nums2[j];  
             j++;
             k++;
         }
-        
-        // Find and return the median
         int n = result.length;
         if(n % 2 == 1) {
-            return result[n / 2]; // Odd case
+            return result[n / 2]; 
         } else {
-            // Even case → average of two middle numbers
             return (result[n / 2 - 1] + result[n / 2]) / 2.0;
         }
     }
