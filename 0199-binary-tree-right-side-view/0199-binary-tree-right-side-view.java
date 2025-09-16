9fcea1d262da33c0ed75c03c6d0b4694636b3858
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-09-17 00:43:45 +0530

    Time: 0 ms (100%), Space: 42.6 MB (8.6%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,45 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+    public void rightView(TreeNode root, List<Integer> result, int currentDepth) {
+        if (root == null) {
+            return;
+        }
+        
+        if (currentDepth == result.size()) {
+            result.add(root.val);
+        }
+        
+        rightView(root.right, result, currentDepth + 1);
+        rightView(root.left, result, currentDepth + 1);
+    }
+    
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
-
-        List<TreeNode> queue = new ArrayList<>();
-        queue.add(root);
-
-        int index = 0;
-        while (index < queue.size()) {
-            int levelSize = queue.size() - index;
-
-            List<TreeNode> currentLevelNodes = new ArrayList<>();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.get(index);
-                currentLevelNodes.add(node);
-                index++;
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
-
-            TreeNode rightmostNode = currentLevelNodes.get(currentLevelNodes.size() - 1);
-            result.add(rightmostNode.val);
-        }
-
+        rightView(root, result, 0);
         return result;
     }
 }
