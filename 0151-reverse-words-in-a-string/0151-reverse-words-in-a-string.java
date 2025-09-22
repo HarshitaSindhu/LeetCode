1b0d3d4b591569e01277fb4e597761123bdd5158
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-09-22 19:56:25 +0530

    Time: 17 ms (6.63%), Space: 45.4 MB (9.74%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,48 +1,47 @@
 class Solution {
-    public static void reverse(String[] words , int size){
-        int i = 0;
-        int j = size-1;
 
-        while(i<j){
-            String temp =words[i];
-            words[i] = words[j];
-            words[j] = temp;
-            i++;
-            j--;
-        }
-    } 
+   public static  void reverse(String[] words , int size){
+    int i = 0;
+    int j = size-1;
+    while(i<j){
+        String temp = words[i];
+        words[i] = words[j];
+        words[j] = temp;
+        i++;
+        j--;
+    }
+   }
+
     public String reverseWords(String s) {
-        int l = s.length();
         String currentWord = "";
-        String words[] = new String[l];
+        int n =   s.length();
         int index= 0;
-        for(int i = 0;i<l;i++){
-            if(s.charAt(i) != ' '){
-                currentWord += s.charAt(i);
-            }
-            else{
-                if(!currentWord.equals("")){
-             words[index] =  currentWord;
-             currentWord="";
-             index++; 
+        String[] words = new String[n];
+       for(int i = 0;i<n;i++){
+        if(s.charAt(i)!=' '){
+         currentWord+=s.charAt(i);
+        }
+        else{
+            if(!currentWord.equals("")){
+              words[index] = currentWord;
+              currentWord ="";
+              index++;
             }
         }
+       }
+        if(!currentWord.equals("")){
+              words[index] = currentWord;
+              index++;
         }
-          if(!currentWord.equals("")){
-             words[index] =  currentWord;
-             index++;
-             }
+        reverse(words , index);
 
-             reverse(words , index);
-
-             String ans ="";
-             for(int i = 0;i<index;i++){
-                ans+=words[i];
-                if(i != index-1){
-                    ans+=" ";
-                }
-             }
-
-             return ans;
+      String ans = "";
+      for(int i = 0;i<index;i++){
+        ans+=words[i];
+        if(i!=index-1){
+            ans+=" ";
+        }
+      }
+      return ans;
     }
 }
