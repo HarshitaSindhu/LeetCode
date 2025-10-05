c925127fea09dfe0e4f6265afcb9c131865f31d6
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-10-05 19:31:59 +0530

    Time: 2 ms (52.37%), Space: 45.9 MB (85.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int[] result = new int[nums1.length + nums2.length];
-        
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] < nums2[j]) {
-                result[k] = nums1[i];
+        int n =nums1.length;
+        int m = nums2.length;
+        int i =0;
+        int j =0;
+        int k =0;
+        int result[]=new int[n+m];
+        while (i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                result[k]= nums1[i];
+
                 i++;
                 k++;
-            } else {  
-                result[k] = nums2[j];
+
+            }
+            else {
+                result[k]=nums2[j];
                 j++;
                 k++;
             }
         }
-
-        while(i < nums1.length) {
-            result[k] = nums1[i]; 
+        while(i<nums1.length){
+            result[k]=nums1[i];
             i++;
             k++;
         }
-
-        while(j < nums2.length) {
-            result[k] = nums2[j];  
+          while(j<nums2.length){
+            result[k]=nums2[j];
             j++;
             k++;
         }
-        int n = result.length;
-        if(n % 2 == 1) {
-            return result[n / 2]; 
-        } else {
-            return (result[n / 2 - 1] + result[n / 2]) / 2.0;
+        
+        int x = result.length;
+        if(x%2==1){
+            return result[x/2];
+
+        }
+        else {
+            // Even case → average of two middle numbers
+            return (result[x / 2 - 1] + result[x / 2]) / 2.0;
         }
     }
 }
