bd35de48617cf57a50f435dd5a02d51c1d1faf59
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-10-11 13:13:26 +0530

    Time: 0 ms (100%), Space: 44.7 MB (30.96%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,42 +15,44 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null){
-            return null;
-        }
+        if (head == null) return null;
+
+        // STEP 1: Insert copy nodes in between original nodes
         Node temp = head;
-        while(temp!= null){
+        while (temp != null) {
             Node nextElement = temp.next;
             Node copy = new Node(temp.val);
-            temp.next= copy;
+
+            temp.next = copy;
             copy.next = nextElement;
+
             temp = nextElement;
         }
 
-
+        // STEP 2: Connect random pointers of copied nodes
         temp = head;
-        while(temp!= null){
+        while (temp != null) {
             Node copyNode = temp.next;
-            if(temp.random != null){
-                copyNode.random  = temp.random.next;
-
-            }
-            else{
+            if (temp.random != null) {
+                copyNode.random = temp.random.next;
+            } else {
                 copyNode.random = null;
             }
-            temp = temp.next.next;
+            temp = temp.next.next; // move to next original
         }
 
+        // STEP 3: Separate the two lists (original + copy)
         temp = head;
         Node dummy = new Node(-1);
         Node copyHead = dummy;
-
-        while(temp != null){
+        while (temp != null) {
             copyHead.next = temp.next;
             copyHead = copyHead.next;
-            temp.next = temp.next.next;
+
+            temp.next = temp.next.next; // restore original
             temp = temp.next;
         }
-        return dummy.next;
+
+        return dummy.next; // head of cloned list
     }
 }
