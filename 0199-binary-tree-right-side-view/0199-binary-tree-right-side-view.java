cca22809954e7322dea4a406f0c97a8c1251f51c
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-11-05 19:42:39 +0530

    Time: 0 ms (100%), Space: 43.6 MB (5.39%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,23 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public void rightView(TreeNode root, List<Integer> result, int currentDepth) {
-        if (root == null) {
-            return;
-        }
-        
-        if (currentDepth == result.size()) {
-            result.add(root.val);
-        }
-        
-        rightView(root.right, result, currentDepth + 1);
-        rightView(root.left, result, currentDepth + 1);
+
+
+   public void rightView(TreeNode root , List<Integer> result , int currentDepth){
+    if(root== null){
+        return;
     }
-    
+    if(currentDepth == result.size()){
+        result.add(root.val);
+    }
+    rightView(root.right , result , currentDepth+1);
+    rightView(root.left , result , currentDepth+1);
+   }
+
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        rightView(root, result, 0);
+        rightView(root , result , 0);
         return result;
     }
 }
